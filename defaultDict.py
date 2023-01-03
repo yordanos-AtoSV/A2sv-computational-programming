ce4202e2a765c79c25065df180ad3ccd97a4df68
yordanos-AtoSV <121184143+yordanos-AtoSV@users.noreply.github.com>
2023-01-03 12:28:20 +0300

    Create defaultDict.py

---
+++ defaultDict.py
@@ -0,0 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import defaultdict
+n,m=map(int,input().split())
+
+d=defaultdict(list)
+for i in range(n):
+    d[input()].append(i+1)
+for i in range(m):
+    s=input()
+    if s in d:
+        print(*d[s])
+    else:
+        print(-1)
