115c5d77e7631dab6d603240b0f7e09176fea110
yordanos-AtoSV <121184143+yordanos-AtoSV@users.noreply.github.com>
2023-01-03 07:21:09 +0300

    Create sortArrayByParity.py

---
+++ sortArrayByParity.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        
+        i = 0
+        j =0
+        while j < len(nums):
+            if nums[j]%2==0:
+                nums[i],nums[j] = nums[j],nums[i]
+                i+=1
+            j+=1
+        return nums
