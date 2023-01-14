a248a3d5c30fdefe5ce2b88636a141559a533d18
yordanos-AtoSV <121184143+yordanos-AtoSV@users.noreply.github.com>
2023-01-15 00:17:59 +0300

    Create 2460. ApplyOperationstoanArray.py

---
+++ 2460. ApplyOperationstoanArray.py
@@ -0,0 +1,25 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                nums[i] = nums[i]*2
+                nums[i+1] = 0
+                i += 1
+                i = 0
+        i = 0
+        j = 0
+        while j < len(nums):
+            if nums[j] == 0:
+                j += 1
+            else:
+                nums[i] = nums[j]
+                i += 1
+                j += 1
+
+        while i < len(nums):
+            nums[i] = 0
+            i += 1
+
+        return nums
+   
