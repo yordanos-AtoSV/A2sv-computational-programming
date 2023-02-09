9f66923e7dd0c89f0a202f3cf4a73ccd00023bff
yordanos-AtoSV <121184143+yordanos-AtoSV@users.noreply.github.com>
2023-02-09 20:48:56 +0300

    Time: 456 ms (87.76%), Space: 20.8 MB (64.17%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        c,n=0,len(people)
+        '''
+        1,1,1,1
+        '''
+        i,j=0,n-1
+        people.sort()
+        while i<=j:
+            if people[i]+people[j]<=limit:
+                i+=1
+                j-=1
+            else:
+                j-=1
+            c+=1
+        return c
