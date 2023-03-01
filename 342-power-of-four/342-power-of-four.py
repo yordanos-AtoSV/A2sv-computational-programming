f05a013a3c1db3b9863c1c1e8cf53ec96c1de467
yordanos-AtoSV <121184143+yordanos-AtoSV@users.noreply.github.com>
2023-03-01 17:18:38 +0300

    Time: 31 ms (82.58%), Space: 13.8 MB (93.21%) - LeetHub

--- 342-power-of-four/342-power-of-four.py
+++ 342-power-of-four/342-power-of-four.py
@@ -1,4 +1,8 @@
 class Solution:
-  def isPowerOfFour(self, n: int) -> bool:
-  
-    return n > 0 and bin(n).count('1') == 1 and (n - 1) % 3 == 0
+    def isPowerOfFour(self, n: int) -> bool:
+        if n == 1:
+            return True
+        if n < 1:
+            return False
+        
+        return self.isPowerOfFour(n/4)
