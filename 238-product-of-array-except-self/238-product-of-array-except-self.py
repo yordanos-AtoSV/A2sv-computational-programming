fd4b4525f89d6b02436f3d963e901e5b37f25f90
yordanos-AtoSV <121184143+yordanos-AtoSV@users.noreply.github.com>
2023-03-02 20:26:48 +0300

    Time: 233 ms (81.97%), Space: 22.6 MB (13.60%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,19 +1,13 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        ans = []
         backward = [1]
         forward = [1]
         for i in range(len(nums)):
             forward.append(forward[-1] * nums[i] )
             backward.append(backward[-1] * nums[-(i+1)] )
-            
-                
-        
-     
-            
+               
         backward.reverse()
-        
-     
-        ans = []
         
         for i in range(1,len(backward)):
             left = forward[i -1]
