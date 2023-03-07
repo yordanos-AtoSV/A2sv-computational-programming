77ae9c8f741165220cf28d6205a9ce3fd1b3d395
yordanos-AtoSV <121184143+yordanos-AtoSV@users.noreply.github.com>
2023-03-07 14:43:03 +0300

    Time: 33 ms (75.78%), Space: 13.8 MB (72.62%) - LeetHub

--- 71-simplify-path/71-simplify-path.py
+++ 71-simplify-path/71-simplify-path.py
@@ -1,14 +1,17 @@
 class Solution:
       def simplifyPath(self, path: str) -> str:
-        stack = []
-
-        for str in path.split('/'):
-            if str in ('', '.'):
-                continue
-            if str == '..':
-                if stack:
-                    stack.pop()
-            else:
-                stack.append(str)
-
-        return '/' + '/'.join(stack)
+            path = path.split("/")
+            stack = []
+            for c in path:
+                if c == "" or c == ".":
+                    continue
+                elif c =="..":
+                    if stack:
+                        stack.pop()
+                else:
+                    stack.append(c)
+            
+            return "/" + "/".join(stack)
+                
+                
+                
