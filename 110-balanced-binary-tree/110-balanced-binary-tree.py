98b2d870b04beef8ce51f51d7d126d55dd88a8e9
yordanos-AtoSV <121184143+yordanos-AtoSV@users.noreply.github.com>
2023-03-08 14:50:17 +0300

    Time: 52 ms (70.59%), Space: 18.9 MB (8.72%) - LeetHub

---
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        return self.check(root)[0]
+    
+    
+    def check(self, root: Optional[TreeNode]): 
+            
+            if not root:
+                return [True, 0]
+            
+            left = self.check(root.left)
+            right = self.check(root.right)
+            balanced =  (left[0] and right[0]) and (abs(left[1] - right[1]) <= 1)
+            
+            return [balanced, 1 + max(left[1], right[1])]
+        
+            
+            
+                    
+    
+        
+        
