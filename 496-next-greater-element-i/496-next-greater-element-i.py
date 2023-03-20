d15f1c7d8d7f7e1cf78f1fa9f45dc34610bd5b0b
yordanos-AtoSV <121184143+yordanos-AtoSV@users.noreply.github.com>
2023-03-20 19:58:41 +0300

    Time: 79 ms (33.31%), Space: 14.2 MB (8.27%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -1,24 +1,26 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        #empty list for answer
-        ans_arr=[]
-        #loop through nums1
-        for i in nums1:
-            #default value for answer (if no greater val found)
-            ans = -1
-            #find the index of that number within nums2
-            index_two = nums2.index(i)
-            #temp array: slice off the rest of the array after the index
-            temp = nums2[index_two+1:]
-            #loop through temp array
-            for j in temp:
-                #if item in temp is greater than i
-                if j > i:
-                    #make that the answer
-                    ans = j
-                    #break the loop (we only need to find the first greater value)
-                    break
-            #add ans to the answer array - if we havent found a greater val it'll still be -1
-            ans_arr.append(ans)
-        #bring it home
-        return ans_arr
+        ans = [-1] * len(nums1)
+        stack = []
+        
+        dic = defaultdict(int)
+        for i in range(len(nums1)):
+            dic[nums1[i]] = i
+
+        for c in nums2:
+            if len(stack) != 0:
+                if stack[-1] > c:
+                    stack.append(c)
+                else:
+                    while len(stack) != 0 and stack[-1] < c:
+                        if stack[-1] in nums1:
+                                ans[dic[stack[-1]]] = c
+                          
+                        stack.pop()
+                
+            stack.append(c)
+            
+        return ans
+        
+                        
+                    
