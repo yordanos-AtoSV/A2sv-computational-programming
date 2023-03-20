f553a06a8c5261a05fc0d8c06225c67d0bf52903
yordanos-AtoSV <121184143+yordanos-AtoSV@users.noreply.github.com>
2023-03-20 22:03:29 +0300

    Time: 9659 ms (5.10%), Space: 258.6 MB (5.10%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -9,9 +9,9 @@ class Solution:
                 return
             
             for j in range(i,len(a)):
-                path.append(a[j])
-                back(k+1, j,path)
-                path.pop()
+                # path.append(a[j])
+                back(k+1, j,path+[a[j]])
+                # path.pop()
         
         for i in range(len(a)):
             back(1,i,[a[i]])
