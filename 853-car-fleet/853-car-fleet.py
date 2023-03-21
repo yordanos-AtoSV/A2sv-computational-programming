f153dd21dee73a197ad0ad52687039acfc9757f0
yordanos-AtoSV <121184143+yordanos-AtoSV@users.noreply.github.com>
2023-03-21 17:51:47 +0300

    Time: 1230 ms (11.12%), Space: 32.9 MB (94.49%) - LeetHub

--- 853-car-fleet/853-car-fleet.py
+++ 853-car-fleet/853-car-fleet.py
@@ -6,6 +6,7 @@ class Solution:
             
             
         cars.sort(reverse = True)
+
         stack = []
         for c in cars:
             stack.append((target - c[0]) / c[1]) 
@@ -14,31 +15,5 @@ class Solution:
                 
         return len(stack)
         
-#         check = []
-#         for c in cars:
-            
-#             check.append((target - c[0]) // c[1])
-        
-#         stack = []
-#         count = 0
-#         flag = 1
-#         for c in check:
-#             if len(stack) != 0:
-#                 if c < stack[-1]:
-#                     stack.pop()
-#                     count += 1
-#                     flag = 0
-#                 else:
-                    
-#                     while len(stack) != 0:
-#                         stack.pop()
-                        
-#                     count += 1
-#                     continue
-                        
-#             stack.append(c)
-                
-#         if flag == 1:
-#             return 1
-#         return count
+
         
