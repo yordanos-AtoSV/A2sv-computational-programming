74ce43b804ff8eceadc4c70c6fa2126049ca17f1
yordanos-AtoSV <121184143+yordanos-AtoSV@users.noreply.github.com>
2023-03-22 23:34:22 +0300

    Time: 505 ms (47.06%), Space: 18.7 MB (84.63%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.py
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.py
@@ -5,9 +5,9 @@ class Solution:
         stack = [-1]
         
         
-        for i in range(len(arr)):
+        for i in range(len(arr)+1):
             if stack[-1] != -1:
-                while stack[-1] != -1 and arr[i] < arr[stack[-1]]:
+                while stack[-1] != -1 and (i == len(arr) or arr[i] < arr[stack[-1]]):
                     mid = stack[-1]
                     l = stack[-2]
                     r = i 
@@ -17,18 +17,6 @@ class Solution:
                     stack.pop()
                 
             stack.append(i)
-  
-     
-        if stack[-1] != -1:
-            
-            while stack[-1] != -1:
-                    mid = stack[-1]
-                    l = stack[-2]
-                    r = len(arr) 
-                    temp = (mid - l) * (r - mid) 
-                    ans += temp * arr[stack[-1]]
-                 
-                    stack.pop()
             
             
         return ans % mod
