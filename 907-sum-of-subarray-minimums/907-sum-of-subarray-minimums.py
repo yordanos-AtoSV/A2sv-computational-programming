f0d2540399076cd8ead38ae537811a047de07dc1
yordanos-AtoSV <121184143+yordanos-AtoSV@users.noreply.github.com>
2023-03-22 23:37:27 +0300

    Time: 528 ms (36.93%), Space: 18.9 MB (33.30%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.py
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.py
@@ -1,11 +1,12 @@
 class Solution:
-    def sumSubarrayMins(self, arr: List[int]) -> int:
+    def sumSubarrayMins(self, arr: List[int]) -> int: 
+        
         mod = pow(10,9) + 7
         ans = 0
         stack = [-1]
         
         
-        for i in range(len(arr)+1):
+        for i in range( len(arr) + 1 ):
             if stack[-1] != -1:
                 while stack[-1] != -1 and (i == len(arr) or arr[i] < arr[stack[-1]]):
                     mid = stack[-1]
