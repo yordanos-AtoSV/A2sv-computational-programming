64517349c7d19a0396a478d136919e237c7723d9
yordanos-AtoSV <121184143+yordanos-AtoSV@users.noreply.github.com>
2023-04-06 19:59:36 +0300

    Time: 2369 ms (6.08%), Space: 13.9 MB (67.01%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.py
+++ 526-beautiful-arrangement/526-beautiful-arrangement.py
@@ -2,24 +2,29 @@ class Solution:
     def countArrangement(self, n: int) -> int:
         path = []
         count = 0
-        sets = set()
+        sets = 0
         
         def back(sets, path):
             nonlocal count
+            
             if len(path) == n:
                 count += 1
                 return 
             for i in range(1, n + 1):
                 
-                if i in sets:
-                    continue
-                else:
+                temp = 1 << i
+                ands = temp & sets
+                if ands == 0:
                     if i % (len(path) + 1) == 0 or (len(path)+1) % i == 0:
                         path.append(i)
-                        sets.add(i)
+                        sets |= temp
                         back(sets, path)
                         path.pop()
-                        sets.remove(i)
+                        sets ^= temp
+                        
+                else:
+                    continue
+                    
                     
         back(sets, path)   
         return count
