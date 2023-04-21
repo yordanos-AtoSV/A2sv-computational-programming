69bb7c9bdcf38b0cd0ddf6a4ae23417afab3faaf
yordanos-AtoSV <121184143+yordanos-AtoSV@users.noreply.github.com>
2023-04-21 20:30:18 +0300

    Time: 43 ms (27.54%), Space: 13.9 MB (64.47%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -4,10 +4,38 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-  def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-    if not list1 or not list2:
-      return list1 if list1 else list2
-    if list1.val > list2.val:
-      list1, list2 = list2, list1
-    list1.next = self.mergeTwoLists(list1.next, list2)
-    return list1
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(-1)
+        temp = dummy
+        cur1 = list1
+        cur2 = list2
+        if not cur1 and not cur2:
+            return 
+        while cur1 and cur2 :
+            if cur1.val < cur2.val:
+                temp.next = ListNode(cur1.val) 
+                temp = temp.next
+             
+                cur1 = cur1.next
+ 
+       
+                    
+            else:
+                temp.next = ListNode(cur2.val) 
+                temp = temp.next
+            
+                cur2 = cur2.next
+        
+            
+        if cur1:
+            while cur1 :
+                temp.next = cur1
+                temp = temp.next
+                cur1 = cur1.next
+        if cur2:
+            while cur2 :
+                temp.next = cur2
+                temp = temp.next
+                cur2 = cur2.next
+            
+        return dummy.next
