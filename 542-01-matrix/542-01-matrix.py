0db20d17c64e39567c243b0bd1c98c0e137c5a9a
yordanos-AtoSV <121184143+yordanos-AtoSV@users.noreply.github.com>
2023-04-30 01:55:25 +0300

    Time: 706 ms (39.52%), Space: 19.1 MB (6.21%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -1,11 +1,11 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        ans = [[0 for _ in range(len(mat[0]))]] * len(mat)
+     
   
         direc = [(0,1), (1,0), (-1, 0), (0, -1)]
         inbound = lambda row, col: 0 <= row < len(mat) and 0 <= col < len(mat[0])
         queue = deque()
-        visited = set()
+
         
         for i in range(len(mat)):
             for j in range(len(mat[0])):
@@ -17,7 +17,7 @@ class Solution:
         
         while queue:
             
-            for _ in range(len(queue)):
+            
                 row, col = queue.popleft()
                 for r, c in direc :
                     nr= row + r
