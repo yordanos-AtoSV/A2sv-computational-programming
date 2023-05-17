501ae43d60b2e11ac9e9149a2747f169efe0f1bf
yordanos-AtoSV <121184143+yordanos-AtoSV@users.noreply.github.com>
2023-05-17 18:39:04 +0300

    Time: 2514 ms (8.96%), Space: 126.3 MB (40.62%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,29 +1,45 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        if destination == source:
-            return True
         
-        adjlist = defaultdict(list)
-        
-        for edge in edges:
-            adjlist[edge[0]].append(edge[1]) 
-            adjlist[edge[1]].append(edge[0])
+        size = {i: 1 for i in range(n)}
+        rep = {i:i for i in range(n)}
             
+        def find(x):
+            if rep[x] != x:
+                rep[x]= find(rep[x])
+                
+            return rep[x]
+
+        def union(x, y):
+            xrep = find(x)
+            yrep = find(y)
+            if xrep != yrep:
+                if size[xrep] > size[yrep]:   
+                    rep[yrep] = xrep
+                    size[xrep] += size[yrep]
+                else:
+                    rep[xrep] = yrep
+                    size[yrep] += size[xrep]
+                        
+        def connected(x, y):
+                return find(x) == find(y)
         
-        visited  = set()
-        stack = []
-        stack.append(source)
         
-        while stack:
-            cur = stack.pop()
+        def solution(source, destination):
+            if destination == source:
+                return True
 
-            for neigh in adjlist[cur]:
-
-                if neigh not in visited :
-                    visited.add(neigh)
-                    stack.append(neigh)
-
-        if destination in visited:
-            return True
             
-        return False
+            adjlist = defaultdict(list)
+
+            for u, v in edges:
+                adjlist[u].append(v) 
+                adjlist[v].append(u)
+                union(u, v)
+            
+            
+            
+            return connected(source, destination)
+
+            
+        return solution(source, destination)
