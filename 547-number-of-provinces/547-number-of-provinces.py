05d5c383de5834aa7639a6d3e80569675d4da418
yordanos-AtoSV <121184143+yordanos-AtoSV@users.noreply.github.com>
2023-05-20 19:50:07 +0300

    Time: 216 ms (17.84%), Space: 16.8 MB (24.21%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,17 +1,40 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        province = 0
-        visited = set()
-        def dfs(start):
-            visited.add(start)
-            for end in range(len(isConnected)):
-                if isConnected[start][end] and end not in visited:
-                    dfs(end)
-            
-                    
-        for start in range(len(isConnected)):
-            if start not in visited:
-                province += 1
-                dfs(start)
+        def find(x):
+            if rep[x] != x:
+                rep[x] = find(rep[x])
+                
+            return rep[x] 
         
-        return province
+        def union(x, y):
+            xrep = find(x)
+            yrep = find(y)
+            
+            if xrep != yrep:
+                if size[xrep] >= size[yrep]:
+                    rep[yrep] = xrep
+                    size[xrep] += size[yrep]
+                    
+                else:
+                    rep[xrep] = yrep
+                    size[yrep] += size[xrep] 
+                    
+        n = len(isConnected)   
+        rep = {i : i for i in range(n )}
+        size = {i : 1 for i in range(n)}
+        
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j] and i != j:
+                    
+                    union(i , j)
+                    
+       
+        parents = set()
+        for i in range( n ):
+            parents.add(find(i))
+            
+            
+        return len(parents)
+            
+        
